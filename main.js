4403cea3ea52bd1ec79a6e6216f8e79c382cc2be
Wate02 <alx.schule@gmail.com>
2024-04-11 21:03:26 +0200

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('Hello World!');
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function discord() {
+  location.href = 'https://www.google.com';
+}
